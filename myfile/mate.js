751a45dd58f481ab4a67371e1f8208df01455057
albert16102002 <127608533+albert16102002@users.noreply.github.com>
2023-06-08 20:27:12 +0630

    project

--- myfile/mate.js
+++ myfile/mate.js
@@ -1,1 +1,29 @@
-console.log("this my new project!")
+const inputTag =document.getElementsByClassName("form-control")[0];
+const shoppingTag=document.getElementsByClassName("ShoppingListContainer")[0];
+
+let productId=1;
+
+const handleChange =(event) => {
+    const inputValue=event.target.value;
+    const parentDiv=document.createElement("div");
+    parentDiv.classList.add("productName");
+    const spanTag=document.createElement("span");
+    const trashIcon=document.createElement("i");
+    trashIcon.classList.add("fa-regular","fa-trash");
+    parentDiv.addEventListener("click", ()=>{
+        const classExist= parentDiv.classList.contains("purchased")
+        if (classExist){
+            parentDiv.classList.remove("purchased")
+        }else{
+            parentDiv.classList.add("purchased")
+        }
+    })
+    spanTag.id = productId;
+    const product = productId.toString()+". " + inputValue;
+    spanTag.append(product);
+    parentDiv.append(spanTag,trashIcon);
+    shoppingTag.append(parentDiv);
+    inputTag.value="";
+    productId += 1;
+}
+inputTag.addEventListener("change",handleChange)
